fb7034f64772f7b1813f44ed61dfb9ceb23a3caa
frigaut <frigaut>
2010-04-15 02:29:32 +0000

    part of the update of this whole repo -> 4.5.1

--- doc/scripts.js
+++ doc/scripts.js
@@ -48,3 +48,16 @@ function toggleDiv(what) {
     }
   }
 } 
+
+
+function HideContent(d) {
+document.getElementById(d).style.display = "none";
+}
+function ShowContent(d) {
+document.getElementById(d).style.display = "block";
+}
+function ReverseDisplay(d) {
+if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
+else { document.getElementById(d).style.display = "none"; }
+}
+
